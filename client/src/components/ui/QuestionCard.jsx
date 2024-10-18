88dcec9fe059f65bfe4904b2a7fec58df1ea9458
nateeth <toaigel@gmail.com>
2024-10-18 14:58:38 +0300

    Промежуточные правки

--- client/src/components/ui/QuestionCard.jsx
+++ client/src/components/ui/QuestionCard.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useParams } from "react-router-dom";
 
 // const testCards = [
 //     {
@@ -24,73 +25,76 @@ import axios from "axios";
 //     },
 //   ];
 
+export default function QuestionCard() {
+  const { themeId } = useParams();
+  const [question, setQuestion] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [answers, setAnswers] = useState([]);
+  const [correctAnswer, setCorrectAnswer] = useState(null);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
 
-  export default function QuestionCard({ themeId }) {
-    const [cards, setCards] = useState([]); 
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null); 
-  
-    useEffect(() => {
-      const fetchCards = async () => {
-        try {
-          const response = await axios.get(`http://localhost:3000/api/:ThemeId`); 
-          setCards(response.data); 
-        } catch (err) {
-          setError("Ошибка при загрузке вопросов"); 
-        } finally {
-          setLoading(false); 
-        }
-      };
-  
-      fetchCards(); 
-    }, [themeId]); 
-  
-    if (loading) return <div>Загрузка...</div>; // добавить лоадер потом
-    if (error) return <div>{error}</div>; 
-  
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3000/api/${themeId}`
+        );
+        setQuestion(response.data);
+        
+        const allAnswers = [
+          response.data.Ans1,
+          response.data.Ans2,
+          response.data.Ans3,
+          response.data.RightAns,
+        ];
+
+        setAnswers(mixAnswers(allAnswers));
+        setCorrectAnswer(response.data.RightAns);
+      } catch (err) {
+        setError("Ошибка при загрузке вопросов", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchQuestions();
+  }, [themeId]);
+
+  const mixAnswers = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
+  if (loading) return <div>Загрузка...</div>; // добавить лоадер потом
+  if (error) return <div>{error}</div>;
+
   return (
     <div
       className="card text-white bg-dark"
-      style={{ 
-        margin: "20px", 
-        borderRadius: "10px", 
-        boxShadow: "0 0 15px rgba(255, 255, 255, 0.6)"
+      style={{
+        margin: "20px",
+        borderRadius: "10px",
+        boxShadow: "0 0 15px rgba(255, 255, 255, 0.6)",
       }}
     >
       <div className="card-body">
         <h5 className="card-title text-center">Nighthawk Trivia</h5>
-        <p className="card-text text-center display-6">
-          Do common nighthawks make nests?
-        </p>
+        <p className="card-text text-center display-6">{question.text}</p>
         <div className="d-flex flex-column align-items-center">
-          <a
-            href="#"
-            className="btn btn-outline-light mb-2"
-            style={{ width: "80%" }}
-          >
-            Да
-          </a>
-          <a
-            href="#"
-            className="btn btn-outline-light mb-2"
-            style={{ width: "80%" }}
-          >
-            Нет
-          </a>
-          <a
-            href="#"
-            className="btn btn-outline-light mb-2"
-            style={{ width: "80%" }}
-          >
-            Третий вариант ответа
-          </a>
-          <a
-            href="#"
-            className="btn btn-outline-light mb-2"
-            style={{ width: "80%" }}
-          >
-            Четвертый вариант ответа
-          </a>
+          {answers.map((answer, index) => (
+            <a
+              key={index}
+              href="#"
+              className="btn btn-outline-light mb-2"
+              style={{ width: "80%" }}
+            >
+              {answer}
+            </a>
+          ))}
         </div>
       </div>
     </div>
